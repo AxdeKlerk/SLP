49fcfe82338a17e3d42bfcd845c88c2e9fe4991f
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-08-15 14:06:50 +0100

    Fix url path routings for config and core app

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,3 +1,4 @@
+# apps/core/urls.py
 from django.urls import path
 from . import views
 
@@ -7,4 +8,4 @@ urlpatterns = [
     path('', views.home_view, name='home'),
     path('about/', views.about_view, name='about'),
     path('contact/', views.contact_view, name='contact'),
-    ]
+]
--- config/urls.py
+++ config/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# config/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from apps.core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.core.urls', namespace='core')),
     path('user/', include('apps.user.urls', namespace='user')),
 ]
+
