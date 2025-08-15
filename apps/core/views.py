c53fea9fd2906dba1fa3d89d8aed90c58b6acfd7
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-08-15 13:44:46 +0100

    Fix render request to include 'core' in core app views

--- apps/core/views.py
+++ apps/core/views.py
@@ -4,10 +4,10 @@ from django.views import View
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'home.html')
+    return render(request, 'core/home.html')
 
 def about_view(request):
-    return render(request, 'about.html')
+    return render(request, 'core/about.html')
 
 def contact_view(request):
-    return render(request, 'contact.html')
+    return render(request, 'core/contact.html')
