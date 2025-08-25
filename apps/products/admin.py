47ba2ad27b1b128c44a3ce9f8c4f112ae8487217
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-08-25 14:03:20 +0100

    Add and register artist admin, event admin in admin.py Add new supporting artists to model Style events card

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -3,11 +3,18 @@ from .models import Event, Artist, Venue
 
 # Register your models here.
 
-@admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ("artist", "venue", "gig_date", "start_time", "genre", "price")
     list_filter = ("gig_date", "venue", "artist", "age", "genre")
     search_fields = ("artist__name", "venue__name", "genre")
+    autocomplete_fields = ["artist", "supporting_artists"]
 
-admin.site.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+class EventAdmin(admin.ModelAdmin):
+    filter_horizontal = ('supporting_artists',)
+
+admin.site.register(Artist, ArtistAdmin)
 admin.site.register(Venue)
+admin.site.register(Event, EventAdmin)
--- apps/products/views.py
+++ apps/products/views.py
@@ -20,3 +20,4 @@ class VenueDetailView(DetailView):
     model = Venue
     template_name = 'venue.html' 
     context_object_name = 'venue'
+
