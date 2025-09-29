15176c515517a4b103eb2dcbd9761d9b584c1fa8
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-09-29 17:46:04 +0100

    Fix Square SDK import for client object

--- apps/payments/utils.py
+++ apps/payments/utils.py
@@ -1,8 +1,8 @@
 import os
-from square.client import Client
+from square.client import client
 
 # Initialise Square client
-client = Client(
+client = client.Client(
     access_token=os.getenv("SQUARE_ACCESS_TOKEN"),
     environment="sandbox"  # or "production"
 )
