b4829906f3edd8ea877908aa9058e13617249c4f
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-09-29 18:08:17 +0100

    Clean Square SDK utils setup

--- apps/payments/utils.py
+++ apps/payments/utils.py
@@ -1,8 +1,10 @@
 import os
 from square.client import Square, SquareEnvironment
 
-# Initialise Square client
+# Initialise the Square client
+# Uses your Heroku config var: SQUARE_ACCESS_TOKEN
+# Environment should be "SANDBOX" while testing, "PRODUCTION" when live.
 client = Square(
     access_token=os.getenv("SQUARE_ACCESS_TOKEN"),
-    environment=SquareEnvironment.SANDBOX  # or SquareEnvironment.PRODUCTION
+    environment=SquareEnvironment.SANDBOX
 )
